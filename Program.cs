ebb3700e2e9f6995b5fde14f3d08cadc478cf1bd
Aleksey <kukoba.alexey@gmail.com>
2023-07-14 19:37:38 +0300

    finish

--- Program.cs
+++ Program.cs
@@ -3,4 +3,35 @@
 // 7 -> да 
 // 1 -> нет
 
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+bool ValidateWeekday(int number)
+{
+    if (number > 0 && number <= 7)
+    {
+        return true;
+    }
+    Console.WriteLine("Это не день недели.");
+    return false;
+}
+bool ifWeekend(int number)
+{
+    if(number > 5)
+    {
+        return true;
+    }
+    return false;
+}
+
+if(ValidateWeekday(number))
+{
+    if(ifWeekend(number))
+    {
+        Console.WriteLine("-> да");
+    }
+    else
+    {
+        Console.WriteLine("-> нет");
+    }
+}
 
